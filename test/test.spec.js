59e6c5e59d6af557e45c897d5addabebb4b1304c
siddu-nc <sidduram@neemconsulting.com>
2020-08-11 23:56:47 +0530

    Initial commit

---
+++ test/test.spec.js
@@ -0,0 +1,12 @@
+const add = require('../index.js');
+describe("A unit testing suite for checking additional functionality",function(){
+ 
+ it("Addition of two positive numbers",function(){
+   expect(add(2,3)).toBe(5);
+ });
+
+ it("Additions of two negative numbers", function(){
+   expect(add(-1,-2)).not.toBe(0);
+ });
+
+});
